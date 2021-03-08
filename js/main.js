75e3eda7e0a19dd8c94a77c793b41002819608af
Salvatore Trimarchi <path72@gmail.com>
2021-03-08 17:31:02 +0100

    base

---
+++ js/main.js
@@ -0,0 +1,32 @@
+//###################################################### 
+// DYNAMICS
+
+$(document).ready(function() {
+// ********************* doc ready start ***
+
+
+// nav menu mouseover init
+navMenuInit();
+
+
+// *********************** doc ready end ***
+});
+
+//###################################################### 
+// FUNCTIONS
+
+function navMenuInit() {
+
+	$('.nav').last().children('li').mouseenter(function() {
+		$(this).children('.nav_menu').toggleClass('active');
+		$(this).children('a:not(:last-child)').css({ 'color':'var(--black)' });
+	});
+	
+	$('.nav').last().children('li').mouseleave(function() {
+		$(this).children('.nav_menu').toggleClass('active');
+		$(this).children('a:not(:last-child)').css({ 'color':'var(--grey)' });
+	});
+	
+
+}
+
